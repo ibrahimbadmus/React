e564967ab2ff0fda42605c71397c63443693c91f
Ibrahim Badmus <ibrahimbadmus07@gmail.com>
2022-12-19 14:11:16 +0100

    Using state hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Employee from './components/employee'
+import {useState} from 'react'
 
 function App() {
-   const showEmployees = true;
+  const [role, setRole] = useState('Dev')
+    const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>   
+         <input 
+         type='text' 
+         onChange={(e)=> {
+          console.log(e.target.value)
+          setRole(e.target.value);
+         }}
+        />
         <Employee name="Badmus" role ="React Dev."/>
-        <Employee name="Ibrahim"/>
+        <Employee name="Ibrahim" role = {role}/>
         <Employee name="Olaiya"/>
                
         </>
--- src/components/employee.js
+++ src/components/employee.js
@@ -2,7 +2,10 @@ function Employee (props) {
     return ( 
             <>
     <h3>Employee {props.name}</h3>
-    <p>{props.role ? props.role: "no role"}</p>
+
+    {/*<p>{props.role ? props.role: "no role"}</p>*/}
+       
+        {/* The above line of code is similar to the one below*/}
     {props.role ? <p class="role">{props.role}</p> : <p class="norole"> No Role </p>}
 
     </>
